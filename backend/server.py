523ae5931b45590b1d4ea811c454d86e17f1b992
tickBit <marjukosk@gmail.com>
2025-08-13 13:08:19 +0300

    Writing an image with description to backend works

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,4 @@
+from click import File
 from flask import Flask, request, jsonify, session, redirect, url_for
 from flask_cors import CORS
 import os
@@ -17,7 +18,8 @@ create_table_images = """
 CREATE TABLE images (
     id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
-    file_data BYTEA NOT NULL
+    file_data BYTEA NOT NULL,
+    userId BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE
 )
 """
 
@@ -50,12 +52,12 @@ def connection():
         print("Database connection established")
         return conn
 
-conn = connection()
-cur = conn.cursor()
+#conn = connection()
+#cur = conn.cursor()
 
-# cur.execute(create_table_users)
-# conn.commit()
-conn.close()
+#cur.execute(create_table_images)
+#conn.commit()
+#conn.close()
 
 def auth(f):
     
@@ -73,6 +75,20 @@ def mainpage():
     
 
     return jsonify({'success': True}), 200
+
+@app.route("/deleteme", methods=["GET"])
+@auth
+def deleteMe():
+
+    print(session["username"])
+    
+    conn = connection()
+    cur = conn.cursor()
+    cur.execute(f"DELETE FROM users WHERE username='{session["username"]}'")
+    conn.commit()
+    conn.close()
+    
+    return jsonify({'success': True}), 204
 
 @app.route("/register", methods=["POST"])
 def registerUser():
@@ -112,20 +128,47 @@ def loginUser():
     conn = connection()
     cur = conn.cursor()
     cur.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'")
-    print(cur.fetchone())
+    user = tuple(cur.fetchone())
     conn.close()
     
-    session['loggedin'] = "True"
-        
-    return f"{username} {password}"
+    if (username == user[1] and password == user[3]):
+    
+        session['loggedin'] = "True"
+        session['username'] = username
+        session['userId'] = int(user[0])
+        return jsonify({'success': True}), 200
+
+    # Wrong un and/or pw
+    return jsonify({'success': False}), 403
 
 @app.route("/logout", methods=["GET"])
 @auth
 def logout():
     session.pop("loggedin", None)
-
+    session.pop('username', None)
+    
     return jsonify({'success': True}), 200
 
+@app.route("/upload", methods=["POST"])
+@auth
+def upload_file():
+    
+    if request.method == 'POST':
+        file = request.files['file']
+    if file:
+        with open("C:/users/marju/Pictures/Work of AI/Nallet/"+file.filename, "rb") as f:
+            binary_data = bytearray(f.read())
+        
+        description = request.form["description"]
+                
+        conn = connection()
+        cur = conn.cursor()
+        #cur.execute(f'INSERT INTO images VALUES ({int(session["userId"])}, "{description}", """{binary_data}""" )')
+        cur.execute("""INSERT INTO images (userId, description, file_data) VALUES (%s, %s, %s);""", (session["userId"], description, binary_data))
+        conn.commit()
+        conn.close()
+    return jsonify({'success': True}), 201
+    
 if __name__ == '__main__':
     
     app.secret_key = 'Secret_test_key_for_MY_AND_YOUR_EYES_ONLY'
